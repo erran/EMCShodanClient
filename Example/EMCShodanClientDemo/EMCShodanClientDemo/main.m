c1726ea7ab9668688c844efaf4cb89719136ee6d
Erran Carey <me@errancarey.com>
2014-01-05 14:40:23 -0600

    Update prefix to use EMC vs. EC to follow Apple guidelines

--- Example/EMCShodanClientDemo/EMCShodanClientDemo/main.m
+++ Example/EMCShodanClientDemo/EMCShodanClientDemo/main.m
@@ -1,13 +1,13 @@
 //
 //  main.m
-//  ECShodanClientDemo
+//  EMCShodanClientDemo
 //
 //  Created by Erran Carey on 1/5/14.
 //  Copyright (c) 2014 Erran Carey. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import "ECShodanClient.h"
+#import "EMCShodanClient.h"
 
 int main(int argc, const char * argv[])
 {
@@ -15,7 +15,7 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         NSString *apiKey = [[[NSProcessInfo processInfo] environment] objectForKey:@"SHODAN_API_KEY"];
 
-        ECShodanClient *client = [[ECShodanClient alloc] initWithAPIKey:apiKey];
+        EMCShodanClient *client = [[EMCShodanClient alloc] initWithAPIKey:apiKey];
 
         NSLog(@"client info: %@", [client info]);
 
