35d2bcb0906cda3514124a04d52c9b90bca26fe3
Erran Carey <me@errancarey.com>
2014-01-05 04:41:34 -0600

    Create ECShodanClient.xcworkspace and Example/ECShodanClientDemo

---
+++ Example/ECShodanClientDemo/ECShodanClientDemo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ECShodanClientDemo
+//
+//  Created by Erran Carey on 1/5/14.
+//  Copyright (c) 2014 Erran Carey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
